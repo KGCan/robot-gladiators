304d7fb487399e2702b250655bb4bcb260083e57
PacDraft <Kirsten@PacificDraftWorks.com>
2021-10-19 19:52:25 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,14 +4,14 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
-var enemyHealth = Math.floor(Math.random() * 21) + 40;
+var enemyHealth = randomNumber (40,60);
 var enemyAttack = 12;
 //function to generate a random numerc value
-var randomNumber = fucntion () {
-var value = Math.floor(Math.random() * 21) + 40;
+var randomNumber = function (){
+var value = Math.floor(Math.random() * (max - min + 1) + min);
 
 return value;
-}
+};
 
 console.log(enemyNames);
 console.log(enemyNames.length);
@@ -39,8 +39,10 @@ var fight = function(enemyName) {
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = Math.max(0, enemyHealth - playerAttack);
+//generate random damage value based on player's attack power
+var damage = randomNumber(playerAttack - 3, playerAttack);
+
+enemyHealth = Math.max(0,enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -59,7 +61,8 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = Math.max(0, playerHealth - enemyAttack);
+    var damage = randomNumber(enemyAttack - 3, enemyAttack); 
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
